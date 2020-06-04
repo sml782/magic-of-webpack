6b02abcaec9e4e5e609b9705e8deb2636bc4d1d2
sml782 <sml782@163.com>
2020-06-04 18:59:33 +0800

    feat: 通过验证器验证参数

--- loader-simple/cusom-loader/4.validate-param.js
+++ loader-simple/cusom-loader/4.validate-param.js
@@ -6,17 +6,26 @@ const validateOptions = require('schema-utils');
 // {} map sourceMap 源代码映射
 // {} metadata 元数据可以是任何东西, 如 AST , 会传递给下一个 loader 共享解析
 module.exports = function mL(source, map, meta) {
+  // { total: 7 }
   const schema = {
     type: 'object',
     properties: {
       total: {
-        type: 'number'
+        // type: 'number',
+        // type: ['number', 'string'],
+        // 错误描述
+        // description: 'total 必须是个数字',
+        instanceof: 'Number'
       }
-    }
+    },
+    // 是否允许额外的属性
+    additionalProperties: false,
   }
   const options = loaderUtils.getOptions(this);
   // const title = options.title || '默认标题';
   console.log(validateOptions);
-  validateOptions(schema, options, 'Example Loader');
+  validateOptions(schema, options, {
+
+  });
   this.callback(null, source);
 }
