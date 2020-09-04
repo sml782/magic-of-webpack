1efc96113105d4bcb2ae39ea121444c5cfb5c6cf
sml782 <sml782@163.com>
2020-09-04 10:58:09 +0800

    feat: 添加 rollup build

---
+++ packer-contrast/rollup/src/main.js
@@ -0,0 +1,3 @@
+const redundant = (a, b) => a - b;
+const sum = (a, b) => a + b;
+export default sum;
--- packer-contrast/webpack/webpack.config.js
+++ packer-contrast/webpack/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  mode:'development',//开发模式
-  devtool:false,//不生成 sourcemap
-  entry: "./src/app.js",//入口文件
+  mode: 'development', // 开发模式
+  devtool: false, // 不生成 sourcemap
+  entry: './src/app.js', // 入口文件
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
+            presets: ['@babel/preset-env']
           },
         },
-        include: path.join(__dirname, "src"),
+        include: path.join(__dirname, 'src'),
         exclude: /node_modules/,
       },
     ],
