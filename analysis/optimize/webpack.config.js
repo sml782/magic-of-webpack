9dc887694e1f4fbd3f55e9ed2b196f4b03e5ed40
sml782 <sml782@163.com>
2020-09-15 18:10:32 +0800

    feat: optmize test

--- analysis/optimize/webpack.config.js
+++ analysis/optimize/webpack.config.js
@@ -4,9 +4,10 @@ const PurgecssPlugin = require('purgecss-webpack-plugin');
 // const TerserPlugin = require('terser-webpack-plugin');
 // const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const UploadPlugin = require('./plugins/UploadPlugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const glob = require('glob');
+// const UploadPlugin = require('./plugins/UploadPlugin');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -15,58 +16,58 @@ module.exports = {
   mode: 'development',
   devtool: false,
   context: process.cwd(),
-  // entry:'./src/index.js',
-  /*
+  // entry: './src/index.js',
   entry: {
-    page1: "./src/page1.js",
-    page2: "./src/page2.js",
-    page3: "./src/page3.js",
-  }, */
-  entry: {
-    main: './src/index.js',
-    vender: ['lodash'],
+    page1: './src/page1.js',
+    page2: './src/page2.js',
+    page3: './src/page3.js',
   },
+  // entry: {
+  //   main: './src/index.js',
+  //   vender: ['lodash'],
+  // },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
-    // publicPath: "http://img.zhufengpeixun.cn/",
+    // publicPath: 'http://img.zhufengpeixun.cn/',
   },
   optimization: {
     minimize: true,
     minimizer: [
       // 压缩JS
-      /* new TerserPlugin({
-        sourceMap: false,
-        extractComments: false,
-      }),
-      //压缩CSS
-      new OptimizeCSSAssetsPlugin({}), */
+      // new TerserPlugin({
+      //   sourceMap: false,
+      //   extractComments: false,
+      // }),
+      // 压缩CSS
+      // new OptimizeCSSAssetsPlugin({}),
     ],
     // 自动分割第三方模块和公共模块
-    /*  splitChunks: {
-      chunks: "all", //默认作用于异步chunk，值为all/initial/async
-      minSize: 0, //默认值是30kb,代码块的最小尺寸
-      minChunks: 1, //被多少模块共享,在分割之前模块的被引用次数
-      maxAsyncRequests: 2, //限制异步模块内部的并行最大请求数的，说白了你可以理解为是每个import()它里面的最大并行请求数量
-      maxInitialRequests: 3, //限制入口的拆分数量
-      name: true, //打包后的名称，默认是chunk的名字通过分隔符（默认是～）分隔开，如vendor~
-      automaticNameDelimiter: "~", //默认webpack将会使用入口名和代码块的名称生成命名,比如 'vendors~main.js'
+    splitChunks: {
+      chunks: 'all', // 默认作用于异步chunk，值为all/initial/async
+      minSize: 0, // 默认值是30kb,代码块的最小尺寸
+      minChunks: 1, // 被多少模块共享,在分割之前模块的被引用次数
+      maxAsyncRequests: 2, // 限制异步模块内部的并行最大请求数的，说白了你可以理解为是每个import()它里面的最大并行请求数量
+      maxInitialRequests: 3, // 限制入口的拆分数量
+      name: true, // 打包后的名称，默认是chunk的名字通过分隔符（默认是～）分隔开，如vendor~
+      automaticNameDelimiter: '~', // 默认webpack将会使用入口名和代码块的名称生成命名,比如 'vendors~main.js'
       cacheGroups: {
-        //设置缓存组用来抽取满足不同规则的chunk,下面以生成common为例
+        // 设置缓存组用来抽取满足不同规则的chunk,下面以生成common为例
         vendors: {
-          chunks: "all",
-          test: /node_modules/, //条件
-          priority: -10, ///优先级，一个chunk很可能满足多个缓存组，会被抽取到优先级高的缓存组中,为了能够让自定义缓存组有更高的优先级(默认0),默认缓存组的priority属性为负值.
+          chunks: 'all',
+          test: /node_modules/, // 条件
+          // 优先级，一个chunk很可能满足多个缓存组，会被抽取到优先级高的缓存组中,为了能够让自定义缓存组有更高的优先级(默认0),默认缓存组的priority属性为负值.
+          priority: -10,
         },
         commons: {
-          chunks: "all",
-          minSize: 0, //最小提取字节数
-          minChunks: 2, //最少被几个chunk引用
+          chunks: 'all',
+          minSize: 0, // 最小提取字节数
+          minChunks: 2, // 最少被几个chunk引用
           priority: -20,
         },
       },
-    }, */
+    },
     // 为了长期缓存保持运行时代码块是单独的文件
     /*  runtimeChunk: {
       name: (entrypoint) => `runtime-${entrypoint.name}`,
@@ -142,6 +143,7 @@ module.exports = {
     new PurgecssPlugin({
       paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
+    new CleanWebpackPlugin(),
     // new UploadPlugin({}),
     new BundleAnalyzerPlugin({
       analyzerMode: 'disabled', // 不启动展示打包报告的http服务器
